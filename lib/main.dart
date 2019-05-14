068bf909579d75629c3804b08682db5efc34090b
Mayank Khanna <khanna98@outlook.com>
2019-05-15 01:01:36 +0530

    Inital Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:quiz_app/home_page.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      title: "Quiz App",
+      theme: ThemeData(primarySwatch: Colors.pink),
+    );
+  }
+}
